4468eafaea250b9d8ecb2ca93792f9ed00d1a188
marhandamp <marhandam.palindung@gmail.com>
2022-04-17 21:21:35 +0800

    show image

--- app/src/main/java/com/app/myvoting/adapter/KandidatAdapter.kt
+++ app/src/main/java/com/app/myvoting/adapter/KandidatAdapter.kt
@@ -13,16 +13,17 @@ import com.app.myvoting.databinding.ItemKandidatBinding
 import com.bumptech.glide.Glide
 
 class KandidatAdapter(private val context: Context, private val kandidat: List<ModelResult>, private val id: Int, private val noTps: String) : RecyclerView.Adapter<KandidatAdapter.KandidatViewHolder>(){
-//    private var img = listOf<Int>(R.drawable.one, R.drawable.two, R.drawable.tree,)
+    private var img = listOf<Int>(R.drawable.one, R.drawable.two, R.drawable.tree,)
     inner class KandidatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val binding = ItemKandidatBinding.bind(itemView)
-        var baseUrl = "http://voting.kpupangkep.my.id/"
+//        var baseUrl = "http://voting.kpupangkep.my.id/"
         fun bindView(kandidat: ModelResult, id: Int, position: Int, noTps: String){
-            var img = baseUrl+kandidat.img
-//            binding.imgItemPhoto.setImageResource(img[position])
-            Glide.with(context)
-                .load(img)
-                .into(binding.imgItemPhoto)
+//            var img = baseUrl+kandidat.img
+//            Glide.with(context)
+//                .load(img)
+//                .into(binding.imgItemPhoto)
+
+            binding.imgItemPhoto.setImageResource(img[position])
             binding.tvItemName.text = kandidat.namaKandidat
             binding.tvNoUrut.text = "Nomor Urut: ${kandidat.noUrut}"
             itemView.setOnClickListener{
@@ -33,7 +34,8 @@ class KandidatAdapter(private val context: Context, private val kandidat: List<M
                     it.putExtra(DetailKandidatActivity.VISI_MISI, kandidat.visiMisi)
                     it.putExtra("ID", id)
                     it.putExtra("NO_TPS", noTps)
-                    it.putExtra(DetailKandidatActivity.IMAGE, img)
+//                    it.putExtra(DetailKandidatActivity.IMAGE, img)
+                    it.putExtra(DetailKandidatActivity.IMAGE, img[position])
                     itemView.context.startActivity(it)
                 }
             }
